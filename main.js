3716f108011d462b7c6c32169f363d0e5bc45fd1
Blation <kisn3089@naver.com>
2022-05-01 22:25:11 +0900

    complete

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let news = [];
+let page = 1;
+let total_pages = 0;
 let menuBtn = document.querySelectorAll('.menus button');
 let searchButton = document.getElementById('search-button');
 let url;
@@ -7,6 +9,8 @@ let url;
 const renderData = async () => {
     try {
         let header = new Headers({'x-api-key': 'ldTppI2IrqmzZ1XRZ2qVZ-WQnQCfNlXwrJq9sSwSYfM'});
+        url.searchParams.set('page', page);//&page=
+        console.log('url은', url)
         let response = await fetch(url, {headers:header});
         let data = await response.json();
         if(response.status === 200) {
@@ -14,15 +18,15 @@ const renderData = async () => {
                 throw new Error("검색된 결과값이 없습니다.")
             }
             news = data.articles;
+            total_pages = data.total_pages;
+            page = data.page;
             console.log(news);
+
+            render();
+            pageNation();
         }   else {
             throw new Error(data.message);
         }
-
-        news = data.articles;
-        console.log(news);
-
-        render()
     }   catch(error) {
         console.log("잡힌 에러는 ", error.message);
         errorRender(error.message);
@@ -36,7 +40,7 @@ const getLatestNews = async() => {
     //new URL = url을 분석해줌
     console.log(url);
 
-    renderData()
+    renderData();
 };
 
 // 검색으로 뉴스 찾기
@@ -86,5 +90,62 @@ const errorRender = (message) => {
                      </div>`
     document.getElementById("news-board").innerHTML = errorHTML;
 }
+
+
+
+const pageNation = () => {
+    // page group
+    let pageGroup  = Math.ceil(page/5)
+//last
+    let last = pageGroup*5
+//first
+    let first = last - 4
+    let pagenationHTML = '';
+    if(first >= 6) {
+        pagenationHTML = `<li class="page-item">
+          <a class="page-link" href="#" aria-label="Previous" onclick="moveToPage(${1})">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li><li class="page-item">
+          <a class="page-link" href="#" aria-label="Previous" onclick="moveToPage(${page-1})">
+            <span aria-hidden="true">&lt;</span>
+          </a>
+        </li>`;
+    }
+    // if(page === 1) {
+    //     pagenationHTML = ''
+    // }
+
+
+    if(page === total_pages) {
+        last = total_pages;
+    }
+
+    //first~last 페이지 프린트
+    for(let i = first; i<=last; i++) {
+        pagenationHTML += `<li class="page-item ${page === i ? "active" : ""}"><a class="page-link" href="#" onclick="moveToPage(${i})">${i}</a></li>`
+    }
+    if(last < total_pages) {
+        pagenationHTML += `<li class="page-item">
+          <a class="page-link" href="#" aria-label="Next" onclick="moveToPage(${page + 1})">
+            <span aria-hidden="true">&gt;</span>
+          </a>
+        </li>
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Next" onclick="moveToPage(${total_pages})">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>`;
+    }
+    document.querySelector('.pagination').innerHTML = pagenationHTML;
+}
+
+const moveToPage = (pageNum) => {
+    //1. 이동하고 싶은 페이지를 알아야된다.
+    page = pageNum;
+    //2. 이동하고싶은 페이지를 가지고 api를 다시 호출해주자
+    renderData();
+}
+
 searchButton.addEventListener('click', getUseByKeyWord);
 getLatestNews()
